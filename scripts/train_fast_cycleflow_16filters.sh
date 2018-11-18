21f2509e4cfeed6f49541cd68b4b9a258243c59b
Andrew Kondrich <andrewk1@stanford.edu>
2018-11-17 18:59:37 -0800

    add 16 filters version of cycleflow

---
+++ scripts/train_fast_cycleflow_16filters.sh
@@ -0,0 +1,2 @@
+set -ex
+python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 10 --no_dropout --gpu_ids -1 --display_id -1 --fineSize 64 --ngf 16 --ndf 16 --netG glow --invertible_G
