7f258089a23e090d252117acfbd7fa8bae82fde8
Andrew Kondrich <andrewk1@stanford.edu>
2018-11-17 22:33:45 -0800

    turn on GPU for all runs

--- scripts/train_cycleflow.sh
+++ scripts/train_cycleflow.sh
@@ -1,3 +1,3 @@
 set -ex
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --no_dropout \
---gpu_ids -1 --display_id -1 --netG glow --invertible_G
+python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan \
+--no_dropout --display_id -1 --netG glow --invertible_G
