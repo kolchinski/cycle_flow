228f6a7c73442ce8b507d3803fb8cd2975ca1fef
Andrew Kondrich <andrewk1@stanford.edu>
2018-11-28 10:09:48 -0800

    Set fine and load size

--- scripts/train_cycleflow_affine.sh
+++ scripts/train_cycleflow_affine.sh
@@ -1,3 +1,3 @@
 set -ex
 python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --no_dropout \
---display_id -1 --netG glow --invertible_G --use_affine_layers
+--display_id -1 --loadSize 128 --fineSize 64 --netG glow --invertible_G --use_affine_layers
