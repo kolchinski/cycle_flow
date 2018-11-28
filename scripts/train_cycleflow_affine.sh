f53aa5f8b0f1de451687d3412dc51da265d60cb4
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-11-28 12:59:10 -0800

    Updated train_cycleflow_affine script

--- scripts/train_cycleflow_affine.sh
+++ scripts/train_cycleflow_affine.sh
@@ -1,3 +1,4 @@
 set -ex
 python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --no_dropout \
---display_id -1 --loadSize 128 --fineSize 64 --netG glow --invertible_G --use_affine_layers --use_squeeze_layers
+--display_id -1 --loadSize 128 --fineSize 64 --netG glow --invertible_G --use_affine_layers --use_squeeze_layers \
+--nK 8 --nL 3 --hourglass_architecture
