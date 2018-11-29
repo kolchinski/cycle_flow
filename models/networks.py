e840539ee934d62b3dffe07fa490554f264c3621
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-11-28 21:58:57 -0800

    Updated net init

--- models/networks.py
+++ models/networks.py
@@ -72,7 +72,7 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     print("GPU IDs: {}".format(gpu_ids))
     if len(gpu_ids) > 0:
         assert(torch.cuda.is_available())
-        net.to(gpu_ids[0])
+        net = net.to(gpu_ids[0])
         net = torch.nn.DataParallel(net, gpu_ids)
     init_weights(net, init_type, gain=init_gain)
     return net
